88252392d2c8faf350900c563371233ebc68ae9c
Aymeric Neveu <emploi.neveu@gmail.com>
2018-10-11 09:36:50 +0200

    exercice3JSP3

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function vert1() {
+  document.getElementsById('text').style.color = 'green'
+function rouge1() {
+  document.getElementsById('text').style.color= 'red';
+}
+function bleu1() {
+  document.getElementsById('text').style.color= 'blue';
+}
